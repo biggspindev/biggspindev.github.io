2864fa83c081fdf8749e4567893fd747d74f7f4f
Artzert2215 <gamingmmail@gmail.com>
2020-09-19 18:44:58 +0200

    Update load.js

--- load.js
+++ load.js
@@ -2,7 +2,7 @@ function onload() {
   var curdir = "min"
   var fadeTarget = document.getElementById("loadtext");
   fadeTarget.style.opacity = 1;
-  setInterval(fade, 50);
+  setInterval(fade, 80);
   function fade() {
     console.log(fadeTarget.style.opacity)
     console.log(curdir)
