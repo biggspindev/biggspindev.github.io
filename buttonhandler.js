46e34add598b2e05a492cbb4e8f690fc855236a0
Artzert2215 <gamingmmail@gmail.com>
2020-10-02 21:44:53 +0200

    Fixed title and description issue

--- buttonhandler.js
+++ buttonhandler.js
@@ -3,19 +3,21 @@ function navto(destination) {
 }
 function navtogame(destination, gameid) {
   if (gameid == 0) {
-  localStorage.setItem("title", "Disaster Lab");
-  localStorage.setItem("desc", "Description here lol");
-  localStorage.setItem("buttons", ["Button1", "Button2"]);
-  localStorage.setItem("buttonsdestin", ["Buttondest1", "Buttondest2"]);
+    localStorage.setItem("title", "Disaster Lab");
+    localStorage.setItem("istitleshort", true);
+    localStorage.setItem("desc", "Description here lol");
+    localStorage.setItem("buttons", ["Button1", "Button2"]);
+    localStorage.setItem("buttonsdestin", ["Buttondest1", "Buttondest2"]);
   }
   window.location.href = destination;
 }
-function navtoplatform(destination, platformid) {
-  if (platformid == 0) {
-  localStorage.setItem("title", "Title failed to load");
-  localStorage.setItem("desc", "Desc failed to load");
-  localStorage.setItem("buttons", ["Button1", "Button2"]);
-  localStorage.setItem("buttonsdestin", ["Buttondest1", "Buttondest2"]);
+function navtoplatform(destination, gameid) {
+  if (gameid == 0) {
+    localStorage.setItem("title", "Title here lol");
+    localStorage.setItem("istitleshort", true);
+    localStorage.setItem("desc", "Description here lol");
+    localStorage.setItem("buttons", ["Button1", "Button2"]);
+    localStorage.setItem("buttonsdestin", ["Buttondest1", "Buttondest2"]);
   }
   window.location.href = destination;
 }
