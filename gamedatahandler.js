4646cd386c1c811cc61a12404ac0ce6f6d5e4a6e
Artzert2215 <gamingmmail@gmail.com>
2020-10-02 21:31:20 +0200

    Updated game data handler

--- gamedatahandler.js
+++ gamedatahandler.js
@@ -1,11 +1,8 @@
 function onload() {
-  var title = localStorage.getItem("title")
-  var desc = localStorage.getItem("desc")
-  var buttons = localStorage.getItem("buttons")
-  var buttonsdestin = localStorage.getItem("buttonsdestin")
-  console.log("Data recieved:")
-  console.log("Title: %s", title)
-  console.log("Desc: %s", desc)
-  console.log("Buttons: %s", buttons)
-  console.log("Buttonsdestins: %s", buttonsdestin)
+  var title = localStorage.getItem("title");
+  var desc = localStorage.getItem("desc");
+  var buttons = localStorage.getItem("buttons");
+  var buttonsdestin = localStorage.getItem("buttonsdestin");
+  document..getElementById("title").innerHTML = title;
+  document..getElementById("title").innerHTML = desc;
 }
