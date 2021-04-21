5ae0bfb3331e34e7e5a6acccf9fe5afe7dcecef3
Artzert2215 <gamingmmail@gmail.com>
2021-04-21 20:40:05 +0200

    Update buttonhandler.js
    Descrpition added

--- buttonhandler.js
+++ buttonhandler.js
@@ -22,17 +22,17 @@ function navtogame(destination, gameid) {
 }
 function navtospecial(destination, id) {
   if (gameid == 0) {
-    localStorage.setItem("title", "Game Status Meaning");
+    localStorage.setItem("title", "Project Status Meaning");
     localStorage.setItem("istitleshort", true);
-    localStorage.setItem("desc", "Description here lol");
-    localStorage.setItem("buttons", ["Button1", "Button2"]);
-    localStorage.setItem("buttonsdestin", ["Buttondest1", "Buttondest2"]);
+    localStorage.setItem("desc", "A project has 4 possible states:<br>-Date: If the satus is a date, the project will release / be finished at that date.<br>-InDev: The project is currently being worked on, and there is no release date yet.<br>-On Hold: This project isnt cancelled, but the is currently no work done on ths project.<br>-Cancelled, this project will forever be unfinished.");
+    localStorage.setItem("buttons", false);
+    localStorage.setItem("buttonsdestin", false);
   } else if (gameid == 1) {
     localStorage.setItem("title", "About Us");
     localStorage.setItem("istitleshort", true);
-    localStorage.setItem("desc", "Description here lol");
-    localStorage.setItem("buttons", ["Button1", "Button2"]);
-    localStorage.setItem("buttonsdestin", ["Buttondest1", "Buttondest2"]);
+    localStorage.setItem("desc", "We are an aspiring team of developpers who have interest in making stuff on the internet, if you are interested in teaming up, contact one of our team members on discord.");
+    localStorage.setItem("buttons", false);
+    localStorage.setItem("buttonsdestin", false);
   } else if (gameid == 2) {
     localStorage.setItem("gtitle", "The Team");
     localStorage.setItem("isteam", true);
