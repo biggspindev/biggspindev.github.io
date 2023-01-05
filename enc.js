bdcbca2029b4f89db25b7fef42d2a7fa0100c398
Michael Peeters <michael.peeters1@student.kuleuven.be>
2023-01-05 16:31:36 +0100

    added encrypted pages system

---
+++ enc.js
@@ -0,0 +1,39 @@
+function decrypt() {
+    let queryString = window.location.search;
+    let urlParams = new URLSearchParams(queryString);
+    let key = String(urlParams.get('key')) 
+    let page = String(location.href.split("/").slice(-1)).split("?")[0]
+    
+    document.getElementById('pageheader').innerHTML = `Viewing encrypted page: <b>${page}</b><br>Using key: <b>${key}</b>`
+
+    children = document.getElementById("contents").children
+    for (let i=0; i < children.length; i++) {
+        let child = children[i]
+        var encrypted = child.innerHTML
+        child.innerHTML = AESdecrypt(key, encrypted);
+    }
+}
+
+const AEScrypt = (salt, text) => {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+
+    return text
+        .split("")
+        .map(textToChars)
+        .map(applySaltToChar)
+        .map(byteHex)
+        .join("");
+};
+  
+const AESdecrypt = (salt, encoded) => {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+    return encoded
+        .match(/.{1,2}/g)
+        .map((hex) => parseInt(hex, 16))
+        .map(applySaltToChar)
+        .map((charCode) => String.fromCharCode(charCode))
+        .join("");
+};
