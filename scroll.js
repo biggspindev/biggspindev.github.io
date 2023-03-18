a1de60722207101027f2e37c1b98064d7b60cd26
Michael Peeters <michael.peeters1@student.kuleuven.be>
2023-03-18 13:18:27 +0100

    mobile scrolling for scroll.js
    fixed up some stuff too

--- scroll.js
+++ scroll.js
@@ -8,18 +8,39 @@ function scrollInit() {
     content = document.getElementById("holder")
     contentsize = content.offsetHeight
     if (contentsize/screensize > 0.9) {
+        contentsize = content.offsetHeight - Math.round(0.5 * screensize)
         content.style.transform = "translate(0, 0)"
+
         addEventListener("wheel", (event) => {
             progress += event.deltaY
             if (progress < 0) {progress = 0}
             if (progress > contentsize) {progress = contentsize}
         });
 
+        addEventListener("touchstart", touchStart);
+        addEventListener("touchmove", touchMove);
+
+        var start = 0;
+
+        function touchStart(event) {
+            start = event.touches[0].pageY;
+        }
+
+        function touchMove(event) {
+            offset = start - event.touches[0].pageY;
+            progress += offset
+            if (progress < 0) {progress = 0}
+            if (progress > contentsize) {progress = contentsize}
+        }
+
         setInterval(function(){
+            screensize = window.innerHeight
+            contentsize = content.offsetHeight - Math.round(0.5 * screensize)
+
             dprogress += ((progress - dprogress)/6);
             dprogress = Math.round(dprogress * 100) / 100
-            percProgress = Math.round((dprogress*-100/contentsize) * 100) / 100
-            content.style.top = percProgress + 50 + "%"
+            pxProgress = Math.round(-dprogress) + Math.round(0.3 * screensize)
+            content.style.top = pxProgress + "px"
         }, 20);
     }   
 }
