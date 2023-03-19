0feca6c0883cc2bc687609ccc9a81e6d46de5f6c
Artzert <gamingmmail@gmail.com>
2023-03-19 19:31:23 +0100

    styling and encryption aid

--- enc.js
+++ enc.js
@@ -29,6 +29,29 @@ function encryptPage(key) { // to use when making page
     }
 }
 
+function translateBox() {
+    let input = document.getElementById('inputEnc').value;
+    let key = document.getElementById('keyEnc').value;
+    if (key == null || key == "") {
+        return;
+    }
+    let output = "";
+    var lines = input.split('\n');
+    for(var i = 0;i < lines.length;i++){
+        let line = lines[i]
+        line = line.replaceAll('[[', '<b class=\"sellectme\">')
+        line = line.replaceAll(']]', '<\/b>')
+        let pre = "<p class=\"itemnoclick\">"
+        let post = "<\/p>"
+        if (line.substr(0,2) == "##") {
+            line = line.substr(2)
+            pre = "<p class=\"itemnoclick big\">"
+        }
+        output += pre + AEScrypt(key, line) + post + "\n"
+    }
+    document.getElementById('outputEnc').value = output;
+}
+
 const AEScrypt = (salt, text) => {
     const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
     const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
